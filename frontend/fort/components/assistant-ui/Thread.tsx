cdadead4c7bbd2a0cf9f168fb470dfa9df1ee890
Shmygol_Maksim <sh.maksim343@gmail.com>
2025-11-12 13:02:41 +0500

    ref: Thread

--- frontend/fort/components/assistant-ui/Thread.tsx
+++ frontend/fort/components/assistant-ui/Thread.tsx
@@ -1,27 +1,24 @@
 import {ThreadPrimitive} from "@assistant-ui/react";
-import {FC} from "react";
+
 import AssistantMessage from "@/components/assistant-ui/AssistantMessage";
 import ThreadWelcome from "@/components/assistant-ui/ThreadWelcome";
 import UserMessage from "@/components/assistant-ui/UserMessage";
 import Composer from "@/components/assistant-ui/Composer";
 import ThreadScrollToBottom from "@/components/assistant-ui/ThreadScrollToBottom";
 
-export const Thread: FC<{ stageIndex: number }> = ({stageIndex}) => {
+export default function Thread() {
     return (
         <ThreadPrimitive.Root
             className="bg-background box-border h-full flex flex-col overflow-hidden"
-            style={{
-                ["--thread-max-width" as string]: "42rem",
-            }}
+            style={{["--thread-max-width" as string]: "42rem"}}
         >
-            <ThreadPrimitive.Viewport
-                className="flex h-full flex-col items-center overflow-y-scroll scroll-smooth bg-inherit px-4 pt-8">
+            <ThreadPrimitive.Viewport className="flex h-full flex-col items-center overflow-y-scroll scroll-smooth bg-inherit px-4 pt-8">
                 <ThreadWelcome/>
 
                 <ThreadPrimitive.Messages
                     components={{
                         UserMessage: UserMessage,
-                        AssistantMessage: AssistantMessage,
+                        AssistantMessage: AssistantMessage
                     }}
                 />
 
@@ -29,14 +26,10 @@ export const Thread: FC<{ stageIndex: number }> = ({stageIndex}) => {
                     <div className="min-h-8 flex-grow"/>
                 </ThreadPrimitive.If>
 
-                {stageIndex < 5 && (
-                    <div
-                        className="sticky bottom-0 mt-3 flex w-full max-w-[var(--thread-max-width)] flex-col items-center justify-end rounded-t-lg bg-inherit pb-4">
-                        <ThreadScrollToBottom/>
-                        <Composer/>
-                    </div>
-                )}
-
+                <div className="sticky bottom-0 mt-3 flex w-full max-w-[var(--thread-max-width)] flex-col items-center justify-end rounded-t-lg bg-inherit pb-4">
+                    <ThreadScrollToBottom/>
+                    <Composer/>
+                </div>
             </ThreadPrimitive.Viewport>
         </ThreadPrimitive.Root>
     );
