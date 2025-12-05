960d9c6e3475f625394c9aaf43cd64a3361a5601
Shmygol_Maksim <sh.maksim343@gmail.com>
2025-12-05 22:06:02 +0500

    feat: protection against import errors

--- backend/operations/apps.py
+++ backend/operations/apps.py
@@ -2,8 +2,13 @@ from django.apps import AppConfig
 
 
 class OperationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'operations'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "operations"
+    verbose_name = "Operations"
 
     def ready(self):
-        import operations.signals
+        try:
+            import operations.signals
+        except Exception as exc:
+            from django.core.exceptions import ImproperlyConfigured
+            raise ImproperlyConfigured(f"Ошибка при загрузке signals в приложении 'operations': {exc}")
